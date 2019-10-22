13d341970a7d049c230ec60cb3cf28e696ca1c03
maryfarner18 <maryfarner@gmail.com>
2019-10-22 14:19:21 -0400

    little bit of stuff

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,25 +2,24 @@ class FavoritesController < ApplicationController
 
     def create
         #IF NOT LOGGED IN, REDIRECT TO LOGIN PAGE
-        byebug
         if ! session[:user_id]
             session[:current_fave] = Madlib.find(params[:id].to_i)
 
             #sets return_to to where I am (this specfic madlib)
             session[:return_to] ||= request.referer
-
             redirect_to login_path
+
+        #If logged in, create and add this to your favorites
         else
-            @user = User.find(session[:user_id])
-            Favorite.create(madlib_id: params[:id].to_i, user_id: @user.id)
+            Favorite.find_or_create_by(madlib_id: params[:id].to_i, user_id: current_user.id)
 
-            redirect_to user_favorites(@user)
-
+            redirect_to user_favorites_path(current_user)
         end
 
     end
 
-    def show
-        @favorites = User.find(params[:id]).favorites
+    def index
+        @favorites = current_user.favorites
+
     end
 end
