02660095dde21068d6c586a5b7f0aaa97d719712
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-25 11:50:35 -0400

    fixed favoriting without account -> creation of account redirection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
         if @user.valid?
             @user.save
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to session.delete(:redirect_to) || user_path(@user)
         else
             flash[:errors] = @user.errors.full_messages
             redirect_to new_user_path
