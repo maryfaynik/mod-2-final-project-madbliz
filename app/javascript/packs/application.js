c7b462e2a7b4e4c3287e01cdd8ae18a934862057
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-25 09:01:32 -0400

    busted some buggeroos

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, tru
 
-require jquery3
-require popper
-require bootstrap
+//= require jquery3
+//= require popper
+//= require bootstrap
